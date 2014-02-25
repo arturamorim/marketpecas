9b11f0ab2dce500be2f30d535c5116dab0a48834
Artur <1101227@isep.ipp.pt>
2014-02-25 23:49:52 +0000

    Estudo da linguagem 2

--- registo/make_registration.php
+++ registo/make_registration.php
@@ -22,9 +22,25 @@ $nif=$_POST['nif'];
 $email=$_POST['email'];
 $telefone=$_POST['telefone'];
 $password=$_POST['password'];
-$sql = mysql_query("INSERT INTO users(nome, morada, nif,email,telefone,password) 
-VALUES ('$nome', '$morada', '$nif', '$email', '$telefone', '$password')");
-echo "<center><h1>$nome o seu registo foi efetuado com sucesso</h1>";
+$uploaddir='upload\\';
+$file_path= $_FILES['arquivo']['tmp_name'];
+$file_name= $_FILES['arquivo']['name'];
+$file_size= $_FILES['arquivo']['size'];
+$file_type= $_FILES['arquivo']['type'];
+$uploadfile= $uploaddir. $_FILES['arquivo']['name'];
+
+if($file_type=="txt" || $file_type=="doc" ||$file_type=="docx" || $file_type=="pdf" ){
+	if(move_uploaded_file($_FILES['arquivo']['tmp_name'], $uploadfile)){
+		echo "Arquivo ".$file_name." enviado<br/>";
+		echo "com o tamanho de ".$file_size. " bytes";
+		echo "<br/>".$_FILES['arquivo']['name']; }
+		else{echo "Arquivo não foi possível enviar.";}
+
+	$sql = mysql_query("INSERT INTO users(nome, morada, nif,email,telefone,password,doc) 
+	VALUES ('$nome', '$morada', '$nif', '$email', '$telefone', '$password', 'C:/$file_name')");
+	echo "<center><h1>$nome o seu registo foi efetuado com sucesso</h1>";
+	}
+	else echo "<br/>Tipo de arquivo não suportado "
 ?>
 
 </body>
--- registo/registo.php
+++ registo/registo.php
@@ -6,14 +6,15 @@
 
 
 <body>
-<form name="signup" method="post" action="make_registration.php">
+<form name="signup" method="post" action="make_registration.php" enctype="multipart/form-data" >
 Nome Completo: <input type="text" name="nome" /> <br/>
 Morada:<input type="text" name="morada" /> <br/>
 NIF:<input type="text" name="nif"/> <br/>
 Email:<input type="text" name="email"/> <br/>
 Telefone:<input type="text" name="telefone"/> <br/>
 Password:<input type="password" name="password"/><br/>
-
+<input type=hidden name=MAX_FILE_SIZE value=102400> <!--  102400 bytes-->
+Comprovativo de Atividade:<input type=File name="arquivo" size="20"/><br/>
 <input type="submit" value="Registar"/>
 </body>
 
